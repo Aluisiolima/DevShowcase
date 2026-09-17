66114396d293a951a803316f259d04117c39b42b
Aluisio Lima <aaluisio309@gmail.com>
2026-09-17 15:25:17 -0300

    criado o arquivo para uma futura conexao com o banco de dados

---
+++ src/devshowcase/core/database.py
@@ -0,0 +1,25 @@
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+from abc import ABC, abstractmethod
+
+from devshowcase.core import Settings
+
+
+class PostgresDB(ABC):
+    def __init__(self):
+        self.database_url = Settings().DATABASE_URL
+        self.engine = create_async_engine(
+            url=self.database_url, echo=not Settings().IS_PRODUCTION
+        )
+        self.session_factory = async_sessionmaker(
+            bind=self.engine, class_=AsyncSession, expire_on_commit=False
+        )
+
+    @abstractmethod
+    async def get_session(self):
+        """Get a new database session."""
+        async with self.session_factory() as session:
+            yield session
