0a32064434dd292794b34a5d0df4229691cde1ab
Aluisio Lima <aaluisio309@gmail.com>
2026-09-19 08:27:49 -0300

    configurando o projeto em torno de seus modulos

--- src/devshowcase/app.py
+++ src/devshowcase/app.py
@@ -3,14 +3,10 @@ from devshowcase.router import router
 from devshowcase.core.settings import Settings
 
 
-class DevShowcaseApp(FastAPI):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.router = router
-        self.title = Settings().NAME_PROJECT
-        self.description = Settings().DESCRIPTION
-        self.version = Settings().VERSION
-        self.docs_url = "/docs"
-        self.redoc_url = "/redoc"
+app = FastAPI(
+    title=Settings().NAME_PROJECT,
+    description=Settings().DESCRIPTION,
+    version=Settings().VERSION,
+)
 
-app = DevShowcaseApp()
+app.include_router(router, prefix=Settings().ROOT_PATH)
--- src/devshowcase/router.py
+++ src/devshowcase/router.py
@@ -1,8 +1,19 @@
 from fastapi import APIRouter
 
+
+from devshowcase.project.controller import project_router
+from devshowcase.technology.controller import technology_router
+from devshowcase.feedback.controller import feedback_router
+from devshowcase.profile.controller import profile_router
+
 router = APIRouter()
 
 
 @router.get("/")
 async def read_root():
     return {"message": "Welcome to the Dev Showcase API!"}
+
+router.include_router(project_router)
+router.include_router(technology_router)
+router.include_router(feedback_router)
+router.include_router(profile_router)
